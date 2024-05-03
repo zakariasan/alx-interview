811b499af87a88c0df3097c6fc4a343828e49ac5
zakariasan <elitziko@gmail.com>
2024-05-03 19:07:41 +0100

    duck type unlock me

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """solution in nutshell"""
+    box = [0]
+    box[0] = True
+    for id, item in enumerate(boxes):
+        if not item:
+            continue
+        for key in item:
+            if (key < len(boxes) and key not in box and key != id):
+                box.append(key)
+    if len(box) == len(boxes):
+        return True
+    return False
