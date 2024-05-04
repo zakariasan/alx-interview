4e41d55932d48c75c1da067b6f2d986da3a8b138
zakariasan <elitziko@gmail.com>
2024-05-04 01:29:05 +0100

    duck type unlock me

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,11 +7,11 @@ sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 def canUnlockAll(boxes):
     """solution in nutshell"""
     box = [0]
-    box[0] = True
     for id, item in enumerate(boxes):
         if not item:
             continue
         for key in item:
+            print(key)
             if (key < len(boxes) and key not in box and key != id):
                 box.append(key)
     if len(box) == len(boxes):
