ff3077639def5f6f641cc1c5b1e43261b99d9593
zakariasan <elitziko@gmail.com>
2024-05-10 03:04:32 +0100

    div index

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,24 +8,8 @@ def minOperations(n):
     """
         In a text file, there is a single character H. Your text editor
     """
-    if n <= 1:
+    if n < 2:
         return 0
-    operations = 0
-    current_chars = 1
-    while current_chars < n:
-        if n % current_chars == 0:
-            operations += 2  # Copy All and Paste
-            current_chars *= 2
-        else:
-            divisor = find_largest_divisor(n)
-            operations += divisor // current_chars + 1
-            current_chars = divisor
-    return operations
-
-
-def find_largest_divisor(n):
-    """ try to do it """
-    for i in range(n // 2, 1, -1):
-        if n % i == 0:
-            return i
-    return n
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
