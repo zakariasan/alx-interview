6e8cb40a716e90c9a87fe1c3a3d00f6c3c0c0cc1
zakariasan <elitziko@gmail.com>
2024-06-06 23:55:29 +0100

    starwrs2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -13,9 +13,8 @@ request(url, (err, response, body) => {
   const filmData = JSON.parse(body);
   const characterUrls = filmData.characters;
 
-  // Function to fetch and print character names
   function fetchCharacter(characterUrl, callback) {
-    request(characterUrl, (err, response, body) => {
+    request(characterUrl, (err, _res, body) => {
       if (err) {
         callback(err);
         return;
@@ -26,7 +25,6 @@ request(url, (err, response, body) => {
     });
   }
 
-  // Using a recursive function to ensure order
   function fetchCharactersInOrder(index) {
     if (index >= characterUrls.length) {
       return;
@@ -40,6 +38,5 @@ request(url, (err, response, body) => {
     });
   }
 
-  // Start fetching characters from index 0
   fetchCharactersInOrder(0);
 });
