4f6caac2eb3d87beba943d82b8d8b7157d823a50
zakariasan <elitziko@gmail.com>
2024-07-04 16:38:07 +0100

    already done with error

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,53 +5,48 @@ In a text file, there is a single character H. Your text editor can execute
 
 
 def is_winner(x, nums):
-    """Determine the overall winner after x rounds of the prime game."""
-    def is_prime(n):
-        """Returns True if n is prime, else False."""
-        if n < 2:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    """
+    Determines the winner of a set of prime number removal games.
+       None.
+    """
+    if x <= 0 or nums is None or x != len(nums):
+        return None
 
-    def get_primes_in_range(start, end):
-        """Returns a list of prime numbers between start"""
-        return [n for n in range(start, end + 1) if is_prime(n)]
+    max_num = max(nums)
+    primes_sieve = generate_prime_sieve(max_num)
 
-    maria_wins = 0
     ben_wins = 0
+    maria_wins = 0
 
     for num in nums:
-        rounds_list = list(range(1, num + 1))
-        primes_list = get_primes_in_range(1, num)
-
-        if not primes_list:
+        prime_count = sum(primes_sieve[:num + 1])
+        if prime_count % 2 == 0:
             ben_wins += 1
-            continue
+        else:
+            maria_wins += 1
 
-        maria_turn = True
-
-        while True:
-            if not primes_list:
-                if maria_turn:
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            smallest_prime = primes_list.pop(0)
-            rounds_list = [n for n in rounds_list if n % smallest_prime != 0]
-            primes_list = [p for p in primes_list if p in rounds_list]
-
-            maria_turn = not maria_turn
-
-    if maria_wins > ben_wins:
-        return "Winner: Maria"
-    elif ben_wins > maria_wins:
-        return "Winner: Ben"
+    if ben_wins > maria_wins:
+        return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
     else:
         return None
+
+
+def generate_prime_sieve(n):
+    """
+    Generates a sieve of Eratosthenes up to n.
+        is a prime number, and 0 otherwise.
+    """
+    sieve = [1] * (n + 1)
+    sieve[0] = sieve[1] = 0
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i] == 1:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = 0
+
+    return sieve
 
 
 print("Winner:", is_winner(5, [2, 5, 1, 4, 3]))
