409f18ea6ddc2ebb3d7bb8cdc7104e49e93a0409
zakariasan <elitziko@gmail.com>
2024-07-04 16:33:05 +0100

    game try

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,14 +4,27 @@ In a text file, there is a single character H. Your text editor can execute
 """
 
 
-def determine_winner(game_rounds, numbers):
-    """Determine the winner of the prime game."""
+def is_winner(x, nums):
+    """Determine the overall winner after x rounds of the prime game."""
+    def is_prime(n):
+        """Returns True if n is prime, else False."""
+        if n < 2:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    def get_primes_in_range(start, end):
+        """Returns a list of prime numbers between start"""
+        return [n for n in range(start, end + 1) if is_prime(n)]
+
     maria_wins = 0
     ben_wins = 0
 
-    for number in numbers:
-        rounds_list = list(range(1, number + 1))
-        primes_list = get_primes_in_range(1, number)
+    for num in nums:
+        rounds_list = list(range(1, num + 1))
+        primes_list = get_primes_in_range(1, num)
 
         if not primes_list:
             ben_wins += 1
@@ -28,30 +41,17 @@ def determine_winner(game_rounds, numbers):
                 break
 
             smallest_prime = primes_list.pop(0)
-            rounds_list.remove(smallest_prime)
-
             rounds_list = [n for n in rounds_list if n % smallest_prime != 0]
+            primes_list = [p for p in primes_list if p in rounds_list]
 
             maria_turn = not maria_turn
 
     if maria_wins > ben_wins:
         return "Winner: Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Winner: Ben"
     else:
         return None
 
 
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def get_primes_in_range(start, end):
-    """Returns a list of prime numbers between start and end (inclusive)."""
-    return [n for n in range(start, end + 1) if is_prime(n)]
+print("Winner:", is_winner(5, [2, 5, 1, 4, 3]))
